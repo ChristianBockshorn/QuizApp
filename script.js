8b557adcf5edff053b64f9e48f720a1810866864
Christian Bockshorn <bockshorn@gmx.net>
2023-03-07 17:36:18 +0100

    update

--- script.js
+++ script.js
@@ -90,9 +90,16 @@ let questions = [
     },
 ];
 
+
+let rightQuestions = 0; //Richtige antworten werden gezählt
+
+
 //Globale Variable mit welcher Frage begonnen wird 
 //Fangen mit der Frage 1 an, somit  mit 0,1,2,3....
 let currentQuestion = 0;
+
+let AudioSuccess = new Audio ('audio/success.mp3');
+let AudioFail = new Audio ('audio/wrong.mp3');
 
 
 function init() {
@@ -103,23 +110,66 @@ function init() {
 
 
 function showQuestion() {
-    //Wir machen uns einen container (let question), gehen in das große Array rein (questions) und holen uns das erste Element (currentQuestion) dort raus
-    let question = questions[currentQuestion];
-    //let question = "Wie heißt die Schicht der Atmosphäre, die der Erde am nächsten ist?"
 
-    document.getElementById('questionNumber').innerHTML =currentQuestion + 1;
-    //(Zahl) von 10 Fragen , die Zahl am anfang wird geändert und zählt mit
+    if (gameIsOver()) {
+        showEndScreen();
 
-    document.getElementById('questiontext').innerHTML = question['question'];
-    //Frage = "Wie heißt die Schicht der Atmosphäre, die der Erde am nächsten ist?"
-    document.getElementById('answer_1').innerHTML = question['answer_1'];
-    //"answer_1": "Stratosphäre",
-    document.getElementById('answer_2').innerHTML = question['answer_2'];
-    //"answer_2": "Mesosphäre",
-    document.getElementById('answer_3').innerHTML = question['answer_3'];
-    //"answer_3": "Troposphäre",
-    document.getElementById('answer_4').innerHTML = question['answer_4'];
-    //"answer_4": "Thermosphäre",
+    } else {
+        updateProgressBar();
+        updateToNextquestion();
+    }
+}
+
+
+function gameIsOver() {//gibt aus ob true or false //Cleancode
+    return currentQuestion >= questions.length;
+}
+
+
+function showEndScreen (){
+    document.getElementById('endScreen').style = '';
+    document.getElementById('questionBody').style = 'display: none';
+
+    //Du hast xxx Fragen von (10 (questions.length)) richtig beantwortet. 
+    document.getElementById('amountOfQuestions').innerHTML = questions.length;
+
+    //Du hast (rightQuestions) Fragen von 10 richtig beantwortet.
+    document.getElementById('amountOfRightQuestions').innerHTML = rightQuestions;
+
+    //Bild am ende wird gegen dieses getauscht
+    document.getElementById('headerImage').src = 'img/pexels-win.jpg';
+}
+
+
+function updateProgressBar (){
+    //ladebalken
+    let percent = (currentQuestion + 1) / questions.length;
+    percent = Math.round(percent * 100); //Rundet 
+    document.getElementById('progressBar').innerHTML = `${percent}%`;
+    document.getElementById('progressBar').style = `width: ${percent}%;`;
+    //console.log('fortschritt',percent);
+}
+
+
+function updateToNextquestion(){
+    
+        //Wir machen uns einen container (let question), gehen in das große Array rein (questions) und holen uns das erste Element (currentQuestion) dort raus
+        let question = questions[currentQuestion];
+        //let question = "Wie heißt die Schicht der Atmosphäre, die der Erde am nächsten ist?"
+
+        document.getElementById('questionNumber').innerHTML = currentQuestion + 1;
+        //(Zahl) von 10 Fragen , die Zahl am anfang wird geändert und zählt mit
+
+        document.getElementById('questiontext').innerHTML = question['question'];
+        //Frage = "Wie heißt die Schicht der Atmosphäre, die der Erde am nächsten ist?"
+        document.getElementById('answer_1').innerHTML = question['answer_1'];
+        //"answer_1": "Stratosphäre",
+        document.getElementById('answer_2').innerHTML = question['answer_2'];
+        //"answer_2": "Mesosphäre",
+        document.getElementById('answer_3').innerHTML = question['answer_3'];
+        //"answer_3": "Troposphäre",
+        document.getElementById('answer_4').innerHTML = question['answer_4'];
+        //"answer_4": "Thermosphäre",
 }
 
 
@@ -139,21 +189,29 @@ function answer(selection) { //selection ist _1, _2, _3, _4
     let idOfRightAnswer = `answer_${question['right_answer']}`; //answer_${3}
 
     //Wenn wir auf ein button klicken dann wird gezeigt welches richtig oder falsch ist
-    if (selectedQuestionNumber == question['right_answer']) {
+    if (rightAnswerSelected(selectedQuestionNumber)) {
         console.log('Richtige Antwort!!');
         document.getElementById(selection).parentNode.classList.add('bg-success');
         //.classList ist um eine CSS Klasse hinzuzufügen
         //.parentNode ist um auf das übergeordnete Element(<div>) zu greifen
+        AudioSuccess.play();//Audio abspielen
+        rightQuestions++; //zählt die richtigen antworten
     } else {
         console.log('Falsche Antwort!!');
         document.getElementById(selection).parentNode.classList.add('bg-danger');
 
         document.getElementById(idOfRightAnswer).parentNode.classList.add('bg-success');
         //Die richtige antwort wird angezeigt
+
+        AudioFail.play(); //Audio abspielen
     }
 
     document.getElementById('nextButton').disabled = false;
     //Button wird aktiviert sobald man eine Antwortmöglickeit ausgewählt hat
+}
+
+function rightAnswerSelected (selectedQuestionNumber){
+    return selectedQuestionNumber == question['right_answer']
 }
 
 
@@ -178,4 +236,18 @@ function resetAnswerButtons() {
     document.getElementById('answer_4').parentNode.classList.remove('bg-danger');
     document.getElementById('answer_4').parentNode.classList.remove('bg-success');
 
+}
+
+
+function restartGame() {
+    document.getElementById('headerImage').src = 'img/quiz.jpg';
+    
+    //Variablen werden überschrieben
+    rightQuestions = 0;
+    currentQuestion = 0;
+
+    document.getElementById('questionBody').style = ''; //questionBody wieder anzeigen
+    document.getElementById('endscreen').style = 'display: none'; //endscreen ausblenden
+
+    init();//wieder von neu starten
 }
